5a45be8bc802543e1c995b2d39ec64635707dace
Devansh Gupta <devanshguptamrt@gmail.com>
2020-05-25 14:47:51 +0530

    fixed gateway discovery

--- services/nginx/server/docker.sh
+++ services/nginx/server/docker.sh
@@ -3,7 +3,7 @@
 # Usage, docker.sh :oid :plan :name :domains
 cid=$(docker ps -q -f "label=oid=$1")
 port=$(docker port $cid | awk -F : '{print $2}')
-bindip=$(netstat -r | awk '$1 ~ /default/{print $2}') #Getting default gateway ip addr
+bindip=$(ip route get 8.8.8.8| awk 'NR==1{print $3}') #Getting default gateway ip addr
 # Adding request for new site initialization
 echo "$3;$bindip:$port;$4" >/var/wp/new/site$1
 # Adding for backup if required
