b4d1b9df527726835964b780a1c5d499aec24c06
Devansh Gupta <devanshguptamrt@gmail.com>
2020-05-25 15:21:35 +0530

    bug fixed

--- db/deploy-db.sh
+++ db/deploy-db.sh
@@ -6,7 +6,8 @@ sed "s/\$DOCKER_REG/$DOCKER_REG/" $file >$file
 sed "s/\$DO_SECRET_KEY/$DO_SECRET_KEY/" env.env |
     sed "s/\$DO_ACCESS_KEY/$DO_ACCESS_KEY/" |
     sed "s/\$DO_BUCKET/$DO_BUCKET/" |
-    sed "s/\$DO_BUCKET_NAME/$DO_BUCKET_NAME/"
+    sed "s/\$DO_BUCKET_NAME/$DO_BUCKET_NAME/" |
+   
 >env.env
 
 #Deploying to swarm
