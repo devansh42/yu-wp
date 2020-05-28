f659b7110e3000f2b49c4a977099b53ffbf74bfd
Devansh Gupta <devanshguptamrt@gmail.com>
2020-05-28 18:06:03 +0530

    fixed some bugs

--- build.sh
+++ build.sh
@@ -9,41 +9,28 @@ build() {
     docker push $DOCKER_REG/$1
 }
 
-# db() {
-#     build yu_wp:db db
-# }
-# backend() {
-#     build yu_wp:backend backend
-# }
-# wp() {
-#     build yu_wp:wp backend/pyback/services/wp
-# }
-# nginx() {
-#     build yu_wp:nginx services/nginx
-# }
-# ssl() {
-#     build yu_wp:ssl services/ssl
-# }
-# conf() {
-#     build yu_wp:conf services/server
-# }
 
 if [ $# -lt "1" ]; then
     echo "Please specify build target"
     exit 1
 fi
 
-case $1 in
-nginx | ssl | conf)
-    build yu_wp:$1 services/$1
-    ;;
-db | backend)
-    build yu_wp:$1 $1
-    ;;
-wp)
-    build yu_wp:$1 backend/pyback/services/$1
-    ;;
-*)
-    echo "No Build Config found for given project"
-    ;;
-esac
+for x in $(seq $#); do
+    case $1 in
+    nginx | ssl | conf)
+        build yu_wp:$1 services/$1
+        ;;
+    db | backend)
+        build yu_wp:$1 $1
+        ;;
+    wp)
+        build yu_wp:$1 backend/pyback/services/$1
+        ;;
+    *)
+        echo "No Build Config found for given project"
+        echo "Available options are, "
+        echo "nginx ssl conf db backend wp"
+        ;;
+    esac
+    shift 1
+done
--- deploy.sh
+++ deploy.sh
@@ -9,24 +9,21 @@ stack() {
     docker stack up -c $1 $2
 }
 
-# db() {
-#     stack compose/db.yml stack_db
-# }
+if [ $# -lt "1" ]; then
+    echo "Please specify deploy target"
+    exit 1
+fi
 
-# redis() {
-#     stack compose/redis.yml stack_redis
-# }
-# backend() {
-#     stack compose/backend.yml stack_backend
-# }
-# nginx() {
-#     stack compose/nginx.yml stack_nginx
-# }
-case $1 in
-db | redis | backend | nginx)
-    stack compose/$1.yml stack_$1
-    ;;
-*)
-    echo "No deployment  configuration found"
-    ;;
-esac
+for x in $(seq $#); do
+    case $1 in
+    db | redis | backend | nginx)
+        stack compose/$1.yml stack_$1
+        ;;
+    *)
+        echo "No deployment  configuration found"
+        echo "Potential targets are"
+        echo "db redis backend nginx"
+        ;;
+    esac
+    shift 1
+done
