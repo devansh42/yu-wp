a1f8476391a87c0245a2736a3c7862889284cd5d
Devansh Gupta <devanshguptamrt@gmail.com>
2020-05-31 23:15:50 +0530

    db fixed

--- db/db.sql
+++ db/db.sql
@@ -15,4 +15,4 @@ create database if not exists yu_wp_data;
  1 Site is provisioned successfully
  2 Couldn't Provision site due to error
 **/
-create table if not exists yu_wp_data.orders(oid int,nid varchar(50),site_status tinyint default 0,ssl_status tinyint default 0,otype char(3),domain varchar(50) ,domains varchar(1000));
+create table if not exists yu_wp_data.orders(oid int,nid varchar(50),temp_domain varchar(50),site_status tinyint default 0,ssl_status tinyint default 0,otype char(3),domain varchar(50) ,domains varchar(1000));
