9e1bb2d2bb364739f2c203bc4e913b7696e3fc4e
Devansh Gupta <devanshguptamrt@gmail.com>
2020-06-03 23:59:23 +0530

    bug fixed

--- services/nginx/entrypoint.sh
+++ services/nginx/entrypoint.sh
@@ -9,4 +9,9 @@ bash backup/cron-wp.sh
 nginx -g "daemon off;" & # Starting up nginx
 #echo "Started nginx"
 # Running Worker 
+if [ -e "/etc/nginx/sites-available" ];then
+else
+mkdir /etc/nginx/sites-available
+fi
+
 /worker/worker > /var/log/wp/log.log 2>&1
